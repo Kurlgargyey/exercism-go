a9927a9599f98c91fe731ee82c556c3d97cc3662
Kurlgargyey <nahsirkivar@gmail.com>
2024-05-21 10:35:39 +0200

    passing all blackjack tests

--- blackjack/blackjack.go
+++ blackjack/blackjack.go
@@ -1,12 +1,56 @@
-e package blackjack
+package blackjack
 
 // ParseCard returns the integer value of a card following blackjack ruleset.
 func ParseCard(card string) int {
-	panic("Please implement the ParseCard function")
+	switch card {
+	case "ace":
+		return 11
+	case "two":
+		return 2
+	case "three":
+		return 3
+	case "four":
+		return 4
+	case "five":
+		return 5
+	case "six":
+		return 6
+	case "seven":
+		return 7
+	case "eight":
+		return 8
+	case "nine":
+		return 9
+	case "ten", "jack", "queen", "king":
+		return 10
+	default:
+		return 0
+	}
 }
 
 // FirstTurn returns the decision for the first turn, given two cards of the
 // player and one card of the dealer.
 func FirstTurn(card1, card2, dealerCard string) string {
-	panic("Please implement the FirstTurn function")
+	switch hand := ParseCard(card1) + ParseCard(card2); {
+	case hand < 21 && hand >= 17:
+		return "S"
+	case hand == 22:
+		return "P"
+	case hand == 21:
+		switch ParseCard(dealerCard) < 10 {
+		case true:
+			return "W"
+		default:
+			return "S"
+		}
+	case hand < 17 && hand >= 12:
+		switch ParseCard(dealerCard) >= 7 {
+		case true:
+			return "H"
+		default:
+			return "S"
+		}
+	default:
+		return "H"
+	}
 }
