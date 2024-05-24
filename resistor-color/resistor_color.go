435c8ef384004d96bf2fd51a064cdd852bfb92bb
Kurlgargyey <nahsirkivar@gmail.com>
2024-05-24 11:53:01 +0200

    passing resistor-color

--- resistor-color/resistor_color.go
+++ resistor-color/resistor_color.go
@@ -1,11 +1,13 @@
 package resistorcolor
 
+import "slices"
+
 // Colors returns the list of all colors.
 func Colors() []string {
-	panic("Please implement the Colors function")
+	return []string{"black", "brown", "red", "orange", "yellow", "green", "blue", "violet", "grey", "white"}
 }
 
 // ColorCode returns the resistance value of the given color.
 func ColorCode(color string) int {
-	panic("Please implement the ColorCode function")
+	return slices.Index(Colors(), color)
 }
