71a11a0dbfb5f73b19ad45cefa8b1df5bce3844b
Kurlgargyey <nahsirkivar@gmail.com>
2024-05-27 14:43:41 +0200

    go fmt

--- grains/grains.go
+++ grains/grains.go
@@ -6,14 +6,18 @@ import (
 )
 
 func Square(number int) (uint64, error) {
-	if number <= 0 {return 0, errors.New("no squares have no grains")}
-	if number > 64 {return 0, errors.New("there are only 64 squares on the board")}
+	if number <= 0 {
+		return 0, errors.New("no squares have no grains")
+	}
+	if number > 64 {
+		return 0, errors.New("there are only 64 squares on the board")
+	}
 	return uint64(math.Pow(2, float64(number-1))), nil
 }
 
 func Total() uint64 {
 	var sum uint64 = 0
-	for i:= 1; i <= 64; i++ {
+	for i := 1; i <= 64; i++ {
 		square, _ := Square(i)
 		sum += square
 	}
