5f682b7ed2b09dd267f1d6fcd29b2dc142ecfc4f
Kurlgargyey <nahsirkivar@gmail.com>
2024-05-27 11:56:48 +0200

    passing pangram

---
+++ pangram/pangram.go
@@ -0,0 +1,16 @@
+package pangram
+
+import (
+	"strings"
+	"unicode"
+)
+
+func IsPangram(input string) bool {
+	letters := make(map[rune]int)
+	for _, r := range strings.ToLower(input) {
+		if unicode.IsLetter(r) {
+			letters[r] = 1
+		}
+	}
+	return len(letters) == 26
+}
